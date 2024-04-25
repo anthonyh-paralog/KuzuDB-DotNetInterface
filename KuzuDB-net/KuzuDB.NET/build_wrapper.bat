114003fdc21218704780e11e8e093a12d2a86935
scriptslol <scripts52@gmail.com>
2024-04-24 19:32:37 -0500

    cleanup batch files

--- KuzuDB-net/KuzuDB.NET/build_wrapper.bat
+++ KuzuDB-net/KuzuDB.NET/build_wrapper.bat
@@ -1,13 +1,10 @@
 pushd wrapper
-::mkdir build
-::pushd build
-::%1 -G "Visual Studio 17 2022" -A x64 .. -DCMAKE_BUILD_TYPE=Release
-::%1 --build . --target kuzunet --config Release
+
 %1 -S . -B build
 %1 --build build --target kuzunet --config Release
-::popd
+
 popd
 
-
+:: the following copies should likely live somewhere else
 copy wrapper\build\Release\kuzunet.dll lib\kuzunet.dll
 copy kuzu_shared.dll lib\kuzu_shared.dll
--- KuzuDB-net/KuzuDB.NET/run_swig.bat
+++ KuzuDB-net/KuzuDB.NET/run_swig.bat
@@ -1,17 +1,3 @@
-
-::-I"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\include" ^
-::-I"C:\Program Files (x86)\Windows Kits\10\Include\10.0.18362.0\ucrt" ^
-
-::set SWIG_EXE=%1
-
-::echo %SWIG_EXE%
-
-::-cpperraswarn ^
-
-
-::-namespace KuzuDB ^
-::-dllimport KuzuDB ^
-
 echo %1
 
 %1 ^
